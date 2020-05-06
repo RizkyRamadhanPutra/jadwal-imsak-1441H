a059741e824b9949320a95279c027a778179f00d
RizkyRamadhanPutra <rizkyramadhan2812@gmail.com>
2020-05-06 22:03:41 +0700

    project jadwal imsak

---
+++ app/src/main/java/com/example/projectjadwalimsak/Main3Activity.java
@@ -0,0 +1,15 @@
+package com.example.projectjadwalimsak;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+        getSupportActionBar().setSubtitle("Rizky Ramadhan putra / 311710852 / TI/17.B3");
+    }
+}
